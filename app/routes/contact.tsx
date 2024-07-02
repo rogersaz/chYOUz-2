bb3d6699482a1e44c1372b6c33caf77f8f09c835
Mark <45799393+rogersaz@users.noreply.github.com>
2024-07-02 10:59:53 -0700

    Update contact.tsx

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,14 +1,38 @@
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <form
+        name="contact v1"
+        method="post"
+        data-netlify="true"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input type="hidden" name="form-name" value="contact v1" />
+
+        <div>
+          <label>
+            Name <br />
+            <input type="text" name="full-name" required />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email<br />
+            <input id="email" type="email" name="email" required />
+          </label>
+        </div>
+        <div>
+          <label>
+            Message <br />
+            <textarea name="comments"></textarea>
+          </label>
+        </div>
+        <div>
+          <button type="submit">Send Message</button>
+        </div>
+      </form>
+    </div>
+  );
+}
