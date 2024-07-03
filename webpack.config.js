4155f1c3862e3c00b549320d9009bddff68a05f1
Mark <45799393+rogersaz@users.noreply.github.com>
2024-07-03 07:25:10 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,13 @@
-const path = require('path');
-
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      // Add other loaders here if needed
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.html'],
-  },
-  // Include other configurations as necessary
-};
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: ['html-loader'],
+        },
+        // other rules...
+      ],
+    },
+    // other configurations...
+  };
+  
